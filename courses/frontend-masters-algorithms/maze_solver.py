4216a52ca03ddf4838d9ce86797cb8ad85b220aa
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-06-30 14:27:04 +0200

    automated quick commit

---
+++ courses/frontend-masters-algorithms/maze_solver.py
@@ -0,0 +1,20 @@
+MAZE = [
+    "xxxxxxxxxx x",
+    "x        x x",
+    "x        x x",
+    "x xxxxxxxx x",
+    "x          x",
+    "x xxxxxxxxxx",
+]
+
+def _walk_maze():
+
+
+def solve_maze(maze, start, end):
+    for l in MAZE:
+
+
+if __name__ == "__main__":
+    print(solve_maze(MAZE))
+
+
